b2a9d2c8c2bfb5eae558a702f99d2df3c553d8f4
chizukicn <chizukicn@outlook.com>
2023-11-21 21:02:07 +0800

    chore: update eslint config

--- uno.config.ts
+++ uno.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig, presetUno } from 'unocss'
+import { presetBlock } from './src'
 
 // Just for Vscode Extension
 
 export default defineConfig({
   presets: [
     presetUno(),
+    presetBlock(),
   ],
 })
