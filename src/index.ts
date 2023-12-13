da6938982ae6b30e476a15854a0c85affc09736f
chizukicn <chizukicn@outlook.com>
2023-12-13 09:28:16 +0800

    chore: up deps

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,21 @@ export interface PresetBlockOptions {
   ignores?: BlocklistRule[]
 }
 
+function createFilter(selector: string) {
+  return (rule: RegExp | string | undefined | null | ((raw: string) => boolean | undefined | null)) => {
+    if (typeof rule === 'function')
+      return rule(selector)
+    if (typeof rule === 'string')
+      return selector === rule
+    if (!rule)
+      return false
+
+    return rule.test(selector)
+  }
+}
+
 export const presetBlock = definePreset((_options: PresetBlockOptions = {}) => {
   const blockIgnores = _options.ignores ?? []
-  const createFilter = (selector: string) => {
-    return (rule: BlocklistRule) => {
-      if (typeof rule === 'function')
-        return rule(selector)
-      return !!selector.match(rule)
-    }
-  }
 
   const regexes = [
     /^((p|m|rounded|rd|space|inset)(-?[xyrltb])?|pa|ma|top|bottom|left|right|w|h|min-w|min-h|lh|leading)-?((\.\d+)|(\d+)(\.\d+)?)(rem|px)$/,
